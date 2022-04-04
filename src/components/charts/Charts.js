3e939001007c55760a74b30eb4837a740366388d
Nafiul Alam <nafiulalam30@gmail.com>
2022-04-04 22:36:37 +0600

    charts complete

--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis, ResponsiveContainer, AreaChart, Area } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis, ResponsiveContainer, AreaChart, Area, BarChart, Bar, PieChart, Pie } from 'recharts';
 import './Charts.css'
 
 const Charts = () => {
@@ -8,12 +8,12 @@ const Charts = () => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setData(data));
-    }, [])
+    }, []);
     return (
-        <div>
+        <div className='mb-6'>
             <h1 className='text-3xl text-center font-bold text-slate-700 py-4'>Charts</h1>
-            <div className='md:w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-2 content-center gap-2'>
-                <div>
+            <div className='md:w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-2 content-center gap-2 lg:gap-4'>
+                <div className='-z-10'>
                     <h3 className='text-2xl text-center text-slate-500 font-semibold'>Month wise Sell</h3>
                     <div style={{ width: '100%', height: 400 }}>
                         <ResponsiveContainer>
@@ -30,11 +30,11 @@ const Charts = () => {
                     </div>
                 </div>
                 <div>
-                    <h3 className='text-2xl text-center text-slate-500 font-semibold'>Month wise Sell</h3>
+                    <h3 className='text-2xl text-center text-slate-500 font-semibold'>Investment vs Revenue</h3>
                     <div style={{ width: '100%', height: 400 }}>
                         <ResponsiveContainer>
                             <AreaChart data={data}
-                                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                                margin={{ top: 20, right: 15, left: 5, bottom: 5 }}>
                                 <defs>
                                     <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
                                         <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
@@ -49,14 +49,43 @@ const Charts = () => {
                                 <YAxis />
                                 <CartesianGrid strokeDasharray="3 3" />
                                 <Tooltip />
-                                <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-                                <Area type="monotone" dataKey='revenue' stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                                <Area type="monotone" stackId="1" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                                <Area type="monotone" stackId="1" dataKey='revenue' stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
                             </AreaChart>
                         </ResponsiveContainer>
                     </div>
                 </div>
-                <div></div>
-                <div></div>
+                <div>
+                    <h3 className='text-2xl text-center text-slate-500 font-semibold'>Investment vs Revenue</h3>
+                    <div style={{ width: '100%', height: 400 }}>
+                        <ResponsiveContainer>
+                            <BarChart
+                                data={data}
+                                margin={{ top: 20, right: 15, left: 5, bottom: 5 }}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="month" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                                <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                                <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+                <div>
+                    <h3 className='text-2xl text-center text-slate-500 font-semibold'>Investment vs Revenue</h3>
+                    <div style={{ width: '100%', height: 400 }}>
+                        <ResponsiveContainer>
+                            <PieChart>
+                                <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                                <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                                <Tooltip />
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
